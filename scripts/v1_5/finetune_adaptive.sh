25801a0072083463aa036e95db1915da708ce8a4
liujunhong <liujunhong@kuaishou.com>
2025-08-27 16:48:26 +0800

    modify config

--- scripts/v1_5/finetune_adaptive.sh
+++ scripts/v1_5/finetune_adaptive.sh
@@ -19,12 +19,12 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir ./checkpoints/llava-v1.5-7b-adaptive \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 10 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
