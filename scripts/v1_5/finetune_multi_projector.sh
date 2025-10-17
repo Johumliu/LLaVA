dd9314a42e31650938fce8c95f591f5b1d8b4bd2
liujunhong <liujunhong@kuaishou.com>
2025-10-17 11:54:13 +0800

    modify config

--- scripts/v1_5/finetune_multi_projector.sh
+++ scripts/v1_5/finetune_multi_projector.sh
@@ -7,8 +7,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path ../LLaVA_/playground/data/llava_v1_5_mix665k.json \
+    --image_folder ../LLaVA_/playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
